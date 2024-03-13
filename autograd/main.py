f7b614b7583753429dff7c0644ae4f5447cc6ce1
Gaurab <77386673+gaurabdhg@users.noreply.github.com>
2024-03-13 22:24:01 +0100

    Init

--- autograd/main.py
+++ autograd/main.py
@@ -27,17 +27,38 @@ class Tensor:
         self.data=assertArray(data)
         self.requireGrad=requireGrad
         self.dependsOn-=dependsOn or []
-        self.shape=data.shape
+        self.shape=self.data.shape
         self.grad:Optional['Tensor']=None
+        
+        if self.requireGrad:
+            self.zeroGrad()
+    
     
     def __repr__(self):
         return f'tensor({self.data},requiresGrad={self.requireGrad})'
     
+    def backward(self,grad:'Tensor'=None):
+        assert self.requireGrad,"Called backward on Non-requireGrad Tensor"
+        if grad is None:
+            if self.shape==():
+                grad=Tensor(1)
+            else:
+                raise RuntimeError("Grad must be specified for non-zero tensor")
+            
+        self.grad.data+=grad.data
+        
+        for dependency in self.dependsOn:
+            backGrad=dependency.gradFunc(grad.data)
+            dependency.tensor.backward(Tensor(backGrad))
+    
     def sum(self):
         return tensorSum(self)
     
-def tensorSum(t:Tensor)->Tensor:
-     """
+    def zeroGrad(self):
+        self.grad=Tensor(np.zeros_like(self.data,dtype=np.float64))
+        
+def tensorSum(t):
+    """
     Takes a tensor and returns the 0-tensor
     that's the sum of all its elements.
     """
@@ -52,4 +73,10 @@ def tensorSum(t:Tensor)->Tensor:
     
     return Tensor(sum,requireGrad,dependsOn)
 
-  
+def _add(t1:Tensor,t2:Tensor):
+    data=t1.data+t2.data
+    requireGrad=t1.requireGrad or t2.requireGrad
+    dependsOn:List[Dependency]=[]
+    
+    if t1.requireGrad:
+        
